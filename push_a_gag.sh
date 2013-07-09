fd06d499131fdab610e2487ba2a6561b3943a3ef
Aaron Statham <a.statham@gmail.com>
2013-07-09 20:09:58 +1000

    pushin down a gag

---
+++ push_a_gag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+#grab_a_gag.sh <file/directory name> [target directory ./ default]
+
+#Default server and credentials
+SMBCLIENT="smbclient //Gagri/GRIW -A ~/.smbclient"
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: $(basename $0) {Local file/directory} {Remote target directory}"
+  exit 1
+fi
+
+#Check if $1 exists
+if [ ! -e "$1" ]
+then
+    echo "$1 does not exist"
+    exit 1
+fi
+
+CMD="${SMBCLIENT} -c \"prompt; recurse; cd ${2}; mput ${1}\""
+eval $CMD
+
