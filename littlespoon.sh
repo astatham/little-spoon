c195195f818d0f2b497745d9fc3f8a80cf1800ab
Aaron Statham <a.statham@gmail.com>
2013-08-28 17:52:14 +1000

    Fixed #7 - added -a argument, may be used multiple times

--- littlespoon.sh
+++ littlespoon.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # littlespoon.sh -- Execute a given command in parallel on data stored on a CIFS share.
 #
@@ -20,7 +20,7 @@
 #	<dest CIFS directory> <maximum concurrent tasks> <command>
 #
 
-VERSION=0.2
+VERSION=0.3
 LITTLESPOON=`readlink -f "${0%/*}"`
 
 # Argument defaults
@@ -32,15 +32,6 @@ CREDS_FILE="~/.gagri.creds"
 # The smbclient command
 SMBCLIENT_COMMAND="smbclient"
 
-ValidateCommand()
-{
-	echo "ValidateCommand: TODO"
-	# TODO: Make sure that $COMMAND is a valid qsub -q all.q submission
-	# Additionally, it:
-	#   * Should NOT have a name (-N option)
-	#   * Should NOT have any holds (-hold* option)
-}
-
 ValidateScratchSpace()
 {
 	echo "ValidateScratchSpace: TODO"
@@ -49,7 +40,7 @@ ValidateScratchSpace()
 
 
 # Parse the named arguments
-while getopts ":s:N:t:A:R:f:F:" OPTION; do
+while getopts ":s:N:t:A:R:f:F:a:" OPTION; do
 	case $OPTION in
 		s)	SHARE_NAME=$OPTARG 
 			;;
@@ -69,9 +60,13 @@ while getopts ":s:N:t:A:R:f:F:" OPTION; do
 			;;
 		F)	FILTER_REGEX=$OPTARG
 			;;
-		\?)	echo "Unknown option: -$OPTARG" >&2 
+		a)	COMMAND_ARGUMENTS="$COMMAND_ARGUMENTS $OPTARG"
+			;;
+		\?)	echo "Unknown option: -$OPTARG" >&2
+			exit 1
 			;;
 		:)	echo "Option -$OPTARG requires an argument." >&2 
+			exit 1
 			;;
 	esac
 done
@@ -79,7 +74,7 @@ done
 # Parse positional arguments
 OPTIND_INC=$((3 + OPTIND))
 if [ $# -lt $OPTIND_INC ] || [ $# -lt 4 ]; then
-	echo "Usage: bigspoon.sh [-s <CIFS share>] [-N <Job name prefix>] [-t <temp location>] [-A <creds file>] <source CIFS directory> <dest CIFS directory> <maximum concurrent tasks> <command>"
+	echo "Usage: bigspoon.sh [-s <CIFS share>] [-N <Job name prefix>] [-t <temp location>] [-A <creds file>] [-a <argument to command (may be specified multiple times)>]<source CIFS directory> <dest CIFS directory> <maximum concurrent tasks> <command>"
 	exit 1
 fi
 
@@ -92,18 +87,6 @@ DEST_CIFS_DIR=${OPT_ARRAY[@]:$OPTIND:1}
 NUM_CONCURRENT_TASKS=${OPT_ARRAY[@]:$OPTIND+1:1}
 COMMAND_ARGS=${OPT_ARRAY[@]:$OPTIND+2:${#OPT_ARRAY[@]}-$OPTIND+1}
 COMMAND="${COMMAND_ARGS[*]}"
-
-# Check the command -- it must be a valid qsub -q all.q submission command
-ValidateCommand
-
-# Figure out the command type
-if [ ${COMMAND_ARGS[0]} == "qsub -q all.q" ]; then
-	echo "Type qsub -q all.q"
-	COMMAND_TYPE="qsub -q all.q"
-else
-	echo "Type script"
-	COMMAND_TYPE="script"
-fi
 
 # Check the scratch space
 ValidateScratchSpace
@@ -135,7 +118,7 @@ echo "Directory listing: ${CIFS_DIR_LISTING[@]}"
 
 # Submit the tasks.  Tasks are structured as follows:
 #   1) Fetch job, CIFS --> scratch space.  This may be held on the put job of a previous task.
-#   2) Execute job.  In this case, aarsta just wants command to be executed, assuming it's a qsub -q all.q command.  This is held on the fetch job.
+#   2) Execute job.  In this case, aarsta just wants command to be executed. This is held on the fetch job.
 #   3) Put job, scratch --> CIFS.  This is held on the execute job.
 #   4) Clean up by rm -r scratch/*
 
@@ -150,13 +133,12 @@ for TASK_DIRECTORY in "${CIFS_DIR_LISTING[@]}"; do
 
 	echo "TASK_INDEX=$TASK_INDEX"
 	echo "TASK_DIRECTORY=$TASK_DIRECTORY"
-	
 	# 1) Fetch job
 	# Prepare the scratch space
 	echo "  Preparing scratch"
 	THIS_SCRATCH_PATH=$SCRATCH_PATH/$TASK_DIRECTORY
 	mkdir -p $THIS_SCRATCH_PATH
-	rm $THIS_SCRATCH_PATH/* > /dev/null 2>&1
+	rm -rf $THIS_SCRATCH_PATH/* > /dev/null 2>&1
 	mkdir -p $THIS_SCRATCH_PATH/input $THIS_SCRATCH_PATH/output
 	# Submit the fetch jobs, using Aaron's script
 	echo "  Submitting get jobs (ID $JOB_NAME"_F_"$TASK_INDEX)"
@@ -168,26 +150,18 @@ for TASK_DIRECTORY in "${CIFS_DIR_LISTING[@]}"; do
 		qsub -q all.q -wd $THIS_SCRATCH_PATH -pe orte 1 -N $JOB_NAME"_F_"$TASK_INDEX -j y -b y -shell n -hold_jid $JOB_NAME"_C_"$((TASK_INDEX - NUM_CONCURRENT_TASKS)) "$LITTLESPOON"/grab_a_gag.sh "$SRC_CIFS_DIR/$TASK_DIRECTORY/*" "$THIS_SCRATCH_PATH"/input
 	fi
 	
-	# 2) Execute command.  The command can either be a straight qsub -q all.q, in which case
-	# it is modified in-situ to add the required holds and names, or a script.  If
-	# a script, it is expected that this contains one or more qsub -q all.qs.  The qsub -q all.q jobs 
+	# 2) Execute command.  The command is a script. 
+	# It is expected that this contains one or more qsub -q all.qs.  The qsub -q all.q jobs 
 	# are submitted with names and hold_jids so that the first job to be executed
 	# holds waiting for $WAIT_JOB_ID to be completed, and that the last job(s) to be
 	# executed have a name of $EXEC_JOB_ID.
 	echo "  Submitting compute job $JOB_NAME"_E_"$TASK_INDEX, waiting on $JOB_NAME"_F_"$TASK_INDEX"
 	
 	cd $THIS_SCRATCH_PATH
-	if [ $COMMAND_TYPE == "qsub -q all.q" ]; then
-		COMMAND_MOD=${COMMAND/qsub -q all.q /qsub -q all.q -hold_jid $JOB_NAME"_F_"$TASK_INDEX -N $JOB_NAME"_E_"$TASK_INDEX -cwd }
-		eval $COMMAND_MOD
-	elif [ $COMMAND_TYPE == "script" ]; then
-		export WAIT_JOB_ID="$JOB_NAME"_F_"$TASK_INDEX"
-		export EXEC_JOB_ID="$JOB_NAME"_E_"$TASK_INDEX"
-		eval $COMMAND $TASK_DIRECTORY
-	else
-		echo "Invalid command type $COMMAND_TYPE.  This is a bug, please report it." >&2
-		exit 1
-	fi
+	export WAIT_JOB_ID="$JOB_NAME"_F_"$TASK_INDEX"
+	export EXEC_JOB_ID="$JOB_NAME"_E_"$TASK_INDEX"
+    COMMAND_TO_EXEC="$COMMAND $TASK_DIRECTORY $COMMAND_ARGUMENTS"
+	eval $COMMAND_TO_EXEC
 	
 	# 3) Put job.  It is expected that the output of the job is at 
 	# $THIS_SCRATCH_PATH/output and will be put to $DEST_CIFS_DIR/$TASK_DIRECTORY
@@ -198,6 +172,6 @@ for TASK_DIRECTORY in "${CIFS_DIR_LISTING[@]}"; do
 	echo "  Submitting cleanup job $JOB_NAME"_C_"$TASK_INDEX, waiting on $JOB_NAME"_P_"$TASK_INDEX"
 	qsub -q all.q -wd $SCRATCH_PATH -pe orte 1 -N $JOB_NAME"_C_"$TASK_INDEX -j y -b y -shell n -hold_jid $JOB_NAME"_P_"$TASK_INDEX rm -r "$THIS_SCRATCH_PATH"
 	
-	(( TASK_INDEX++ ))
+    TASK_INDEX=$[$TASK_INDEX +1]
 done
 
